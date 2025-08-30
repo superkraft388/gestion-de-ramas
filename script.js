94982fe00655bcf60f571ae410a3bbdfeb6401aa
superkraft388 <wislinkhonde@hotmail.com>
2025-08-30 01:10:09 -0500

    Add comments to improve code readability

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Fetch the list of branches from the server
 fetch('/branches')
     .then(response => response.json())
     .then(data => {
+        // Get the div element where branches will be displayed
         const branchesDiv = document.getElementById('branches');
+        // Iterate over each branch and create a new div element for it
         data.branches.forEach(branch => {
             const branchElement = document.createElement('div');
             branchElement.textContent = branch;
